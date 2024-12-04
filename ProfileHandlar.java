8048b17e3b122443f38e5ffb844e89ab05cc5a6d
Hafsa Abdalgader <hafos123abdalgader@gmail.com>
2024-12-04 10:41:50 +0100

    Function to add new profile.

---
+++ ProfileHandlar.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProfileHandlar {
+
+
+    public class ProfileHandlar {
+        Scanner scanner = new Scanner(System.in);
+        private final ArrayList<Persons> personList;
+
+        public ProfileHandlar() {
+            personList = new ArrayList<>();
+        }
+
+
+        public void addPerson(Persons person){
+            personList.add(person);
+        }
+
+
+        public void addProfile(Scanner scanner){
+            System.out.println("Enter firstname: ");
+            String firstName = scanner.nextLine();
+            System.out.print("Enter lastname:");
+            String lastName = scanner.nextLine();
+            System.out.print("Enter older: ");
+            int age = scanner.nextInt();
+            scanner.nextLine(); // Konsumera ny rad
+            System.out.print("Enter address: ");
+            String address = scanner.nextLine();
+            System.out.print("Enter telefonnumber: ");
+            String phone = scanner.nextLine();
+
+            personList.add(new Persons(firstName, lastName, age, address, phone));
+            System.out.println("Quit.");}
+
+        public void removeProfile(Scanner scanner) {
+            System.out.print("Enter firstname an profile you will delete: ");
+            String firstName = scanner.nextLine();
+            boolean removed = personList.removeIf(person -> person.getFirstName().equalsIgnoreCase(firstName));
+            if (removed) {
+                System.out.println("Profile removed");
+            } else {
+                System.out.println("No profile with this firstname.");
+            }
+        }
+
+        public void updateProfile(Scanner scanner) {
+            System.out.print("Enter firstname an profile you will update: ");
+            String firstName = scanner.nextLine();
+            for (Persons person : personList) {
+                if (person.getFirstName().equalsIgnoreCase(firstName)) {
+                    System.out.print("Enter new address: ");
+                    String newAddress = scanner.nextLine();
+                    System.out.print("Enter new telefonnubmer: ");
+                    String newPhone = scanner.nextLine();
+                    person.setAddress(newAddress);
+                    person.setPhone(newPhone);
+                    System.out.println("Profile update.");
+                    return;
+                }
+            }
+            System.out.println("No profile with this firstname.");
+        }
+        public void showAllProfiles(){
+            if(personList.isEmpty()) {
+                System.out.println("No profiles.");
+            } else {
+                System.out.println("Profiles:");
+                for (Persons Person : personList){
+                    System.out.println(Persons);
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
